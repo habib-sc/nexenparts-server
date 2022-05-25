8438f58f910a9a3f623b537d57deda3bcc810d8f
habib-sc <habib97.cs@gmail.com>
2022-05-25 23:32:11 +0600

    All user get for admin

--- index.js
+++ index.js
@@ -167,6 +167,13 @@ async function run () {
         } );
 
 
+        // all user get 
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+            const users = await usersCollection.find().toArray();
+            res.send(users);
+        });
+
+
         // Get Orders by user
         app.get('/orders', verifyToken, async (req, res) => {
             const email = req.query.email;  
